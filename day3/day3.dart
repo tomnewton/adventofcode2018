99702c69baf3a4e62a699812fb8704ad99032542
Tom Newton <tom.newton@gmail.com>
2018-12-03 17:27:17 -0500

    clean up

--- day3/day3.dart
+++ day3/day3.dart
@@ -11,10 +11,10 @@ Future<void> main() async {
   // Pattern for the input lines.
   RegExp exp = new RegExp(r"([0-9]+) \@ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)");
 
+  // Will hold all the rects we read in from the input file.
   Map<int, Rectangle> rects = new Map();
 
   // process line by line...
-  List<String> lines = new List();
   await File(path).openRead()
   .transform(Utf8Decoder())
   .transform(new LineSplitter())
@@ -32,16 +32,20 @@ Future<void> main() async {
     rects[int.parse(first.group(1))] = rect;
   });
 
-  // build a bounding box for the cloth
+  // build a bounding box for the cloth based on the rects we read in from input.txt
   Rectangle boundingBox = Rectangle.fromPoints(new Point(0,0), new Point(1,1));
   rects.values.forEach((Rectangle r) => boundingBox = boundingBox.boundingBox(r));
 
+  // Create a two dimensional array to represent our cloth
+  // built from the bounding box's dimensions. 
+  // That is all the bounding box is used for.
   List<List<int>> cloth = new List(boundingBox.width); // cloth[x][y] using a top left 0,0
   for ( var i = 0; i < cloth.length; i++ ) {
     cloth[i] = new List.filled(boundingBox.height, 0);
   }
 
-  int count = 0;
+  int count = 0; // we are going to count squares that are intersected 2 or more times.
+
   rects.values.forEach((Rectangle r){ 
     // fill in the cloth
     for ( var x = r.left; x < r.left + r.width; x++ ) {
@@ -54,12 +58,17 @@ Future<void> main() async {
     }
   });
 
-  print('Count: ${count}'); // 104126
+  print('PART 1: ${count}'); // 104126
 
+  // START OF PART 2
 
   for ( var key in rects.keys ) {
     Rectangle r = rects[key];
     bool success = true;
+    
+    // Loop over this rect on the cloth, and see if it is 
+    // the one that doesn't overlap. It will be the one that doesn't 
+    // overlap if all the squares it covers == 1.
     for ( var x = r.left; x < r.left + r.width; x++ ) {
       for ( var y = r.top; y < r.top + r.height; y++ ) {
         // check each square on the cloth.. if they're all 1s, you're good.
@@ -73,8 +82,8 @@ Future<void> main() async {
       }
     }
 
-    if ( success ) {
-      print('Rectangle ${key} is ok.');
+    if ( success ) { // if you're here, you've found it.
+      print('PART 2: Rectangle ${key}');
       break;
     }
   };
