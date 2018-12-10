91d7096aec20661749b6ba5bdb683530ef7608bc
Tom Newton <tom.newton@gmail.com>
2018-12-10 16:58:03 -0500

    format

--- day5/day5.dart
+++ day5/day5.dart
@@ -7,7 +7,7 @@ import 'package:rxdart/rxdart.dart';
 class Result {
   final String char;
   final int length;
-  
+
   Result(this.char, this.length);
 
   String toString() {
@@ -18,16 +18,19 @@ class Result {
 // Reducer func, processes the stream of characters
 // from the chain.
 String reducer(String prev, String el) {
-  if ( prev == null ) {
+  if (prev == null) {
     return el;
   }
-  String last = prev[prev.length-1];
-  if ( last.toLowerCase() == el.toLowerCase() && last != el ) {
-    if ( prev.length == 1) {
+
+  String last = prev[prev.length - 1];
+
+  if (last.toLowerCase() == el.toLowerCase() && last != el) {
+    if (prev.length == 1) {
       return null;
     }
-    return prev.substring(0, prev.length-1);
+    return prev.substring(0, prev.length - 1);
   }
+
   return prev + el;
 }
 
@@ -38,31 +41,30 @@ Future<void> main() async {
   String path = "${dir}/day5/input.txt";
 
   String code = await File(path)
-  .openRead()
-  .transform(Utf8Decoder())
-  .transform(LineSplitter())
-  .first;
+      .openRead()
+      .transform(Utf8Decoder())
+      .transform(LineSplitter())
+      .first;
 
   List<String> chars = code.split("");
-  
-  String g = await Observable
-  .fromIterable(chars)
-  .reduce( reducer );
+
+  String g = await Observable.fromIterable(chars).reduce(reducer);
 
   // Part 1.
   print(g.length);
 
   Result r = await Observable.fromIterable(chars)
-  .asyncMap((String el) => el.toLowerCase())
-  .distinctUnique()
-  .flatMap((String toRemove) {
+      .asyncMap((String el) => el.toLowerCase())
+      .distinctUnique()
+      .flatMap((String toRemove) {
     return Observable.fromIterable(chars)
-    .where((String el) => el == toRemove || el.toLowerCase() == toRemove ? false : true)
-    .reduce(reducer)
-    .asObservable()
-    .flatMap((String result) => Observable.just(new Result(toRemove, result.length))); 
-  })
-  .min((Result a, Result b) => a.length - b.length);
+        .where((String el) =>
+            el == toRemove || el.toLowerCase() == toRemove ? false : true)
+        .reduce(reducer)
+        .asObservable()
+        .flatMap((String result) =>
+            Observable.just(new Result(toRemove, result.length)));
+  }).min((Result a, Result b) => a.length - b.length);
 
   // Part 2.
   print(r);
